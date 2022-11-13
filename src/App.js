c8b1844b6a0275494a24b02b6220ccdc7e8f4165
Hugodesousa <hugodesousa.16@gmail.com>
2022-11-13 15:15:08 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Home from "./pages/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/App.css'
-import Image from "./components/Image";
+
 function App() {
   return (
     <div>
-      <Image/>
       <Home />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
         <Row>
           <Header />
           <Col>
-            {/* <Image /> */}
+            <Image />
           </Col>
           <Col>
             <Row>
